f71e531eadb2397e3046a61af10a8345918a339d
Andre Dias <andre.dias@concrete.com.br>
2016-10-25 14:53:12 -0200

    Adiciona arquivos inciais do projeto

---
+++ Photorama/Photorama/PhotosViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Andre Dias on 25/10/16.
+//  Copyright © 2016 Andre Dias. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController{
+    
+    @IBOutlet var imageView: UIImageView!
+}
